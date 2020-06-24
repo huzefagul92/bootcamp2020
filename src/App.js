6029cbe2135b1463ee629fabe916293091cea558
Muhammad Huzefa <huzefagul92@gmail.com>
2020-06-24 23:45:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hellow world ddasfsdgfa laucggbhxhchszxf
-    </div>
-  );
-}
 
-export default App;
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+
+    async function getRepose () {
+    const response = await fetch("https//api.github.com/users/huzefagul92")
+    const data = await response.json();
+    console.log(data);
+    setData(data)
+    }
+
+    getRepose();
+  
+  },[])
+//   return (
+//     <div className="App">
+//       Hellow world ddasfsdgfa laucggbhxhchszxf
+//     </div>
+//   );
+// }
+
+// export default App
